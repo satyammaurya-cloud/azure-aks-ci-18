475416874f08b8ab0d266de3134c0bc7e3e04256
Satyam Maurya <satyammaurya1001@gmail.com>
2026-08-19 11:44:35 +0000

    pod-dep

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+resource "azurerm_resource_group" "rgs" {
+  name = "satyam-rg"
+  location = "EAST US"
+}
 
+resource "azurerm_kubernetes_cluster" "dev-aks" {
+  name                = "dev-aks1"
+  location            = azurerm_resource_group.rgs.location
+  resource_group_name = azurerm_resource_group.rgs.name
+  dns_prefix          = "dev-aks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v7"
+  }
+   node_provisioning_profile {
+    mode              = "Auto"             # Enables automatic node management
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
